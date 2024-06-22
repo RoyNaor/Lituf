1fac03c9b89b03862f936493e807a0e4d73c220c
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2024-06-22 18:18:16 +0300

    Add files via upload

---
+++ src/pages/Events.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Events.css'
+
+function Events() {
+  return (
+    <div className='Events'>
+        Events
+        </div>
+  )
+}
+
+export default Events
